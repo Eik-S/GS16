e10b868e32028256f195fe54a91644c28c514ca2
eike <dimitri.x@gmx.de>
2016-07-02 21:37:41 +0200

    Deleted Test line

--- gs/src/gs/VkApiCon.java
+++ gs/src/gs/VkApiCon.java
@@ -176,7 +176,6 @@ public class VkApiCon {
         System.out.println("Hinweis: Sollen mehrere Wörter in einem Post "
 		+ "vorkommen trenne sie bitte durch ein - , \n ansonten "
 		+ "genügt ein Leerzeichen.");
-	System.out.println("Test12");
         input = scanner.nextLine();
         String[] keywords = input.split(" ");
         for(int j = 0; j < ids.length; j++){
